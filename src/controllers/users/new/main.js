406e5ef85f7b64bb190cbefb9ac45f920d0ffd81
RoyaL <orobertofc@gmail.com>
2023-08-24 13:55:44 -0300

    implemented JWTs

--- src/controllers/users/new/main.js
+++ src/controllers/users/new/main.js
@@ -1,23 +1,32 @@
 const { v4: uuidv4 } = require("uuid");
-const bcrypt = require("bcrypt");
-const user_to_db = require("./user_info_to_db");
+const { generateAccessToken, generateRefreshToken } = require("../../../JWT/create_token/create_token");
+const {$transaction} = require("prisma/prisma-client/index-browser");
+const userToDb = require("./user_to_db");
+const tokenToMongoDB = require("../../../JWT/create_token/token_to_mongoDB");
 
-async function main(user_name) {
-  // Generate IDs
-  let private_id = uuidv4();
-  let public_id = uuidv4();
+async function newUser(user_name) {
+  try {
+    const public_id = uuidv4();
+    const private_id = uuidv4();
 
-  // Hash the private ID
-  const saltRounds = 10;
-  let private_id_hashed = bcrypt.hashSync(private_id, saltRounds);
+    const user = {
+      user_name: user_name,
+      public_id: public_id,
+      private_id: private_id,
+    };
 
-  // Send the user to the database
-  if (await user_to_db(user_name, public_id, private_id_hashed)) {
-    return [private_id_hashed, public_id];
-  } else {
-    console.error("There was an error while sending the user to the database.");
-    return "There was an error while sending the user to the database.";
+    const [access_token, refresh_token] = await Promise.all([
+      generateAccessToken(user),
+      generateRefreshToken(user.private_id),
+    ]);
+
+    await userToDb(user);
+
+    return [access_token, refresh_token];
+
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
-}
 
-module.exports = main;
+module.exports = newUser;
