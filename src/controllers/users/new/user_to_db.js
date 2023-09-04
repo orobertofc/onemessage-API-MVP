9cb561373c6ddc1de33741b95178e87aae9a9f91
RoyaL <orobertofc@gmail.com>
2023-09-04 16:03:27 -0300

    "Add password parameter to saveUserToDb function
    Modified the saveUserToDb function to take a password parameter for user creation. Added password field to user details stored in database. Implemented enhanced error handling to manage scenario where username already exists, improving user registration experience."

--- src/controllers/users/new/user_to_db.js
+++ src/controllers/users/new/user_to_db.js
@@ -6,24 +6,32 @@ const { PrismaClient } = require('@prisma/client')
  * @param {object} user - The user object to be saved.
  * @param {string} user.name - The name of the user.
  * @param {number} user.id - The user's ID.
+ * @param {string} password - The user's password.
  * @throws {Error} If there is an error saving the user to the database.
  * @returns {Promise<void>} A Promise that resolves once the user has been saved to the database or rejects with an error.
  */
-async function saveUserToDb(user) {
+async function saveUserToDb(user, password) {
   const prisma = new PrismaClient();
   try {
     await prisma.user.create({
       data: {
         name: user.name,
         id: user.id,
+        password: password,
         lastSeen: new Date(),
       },
     });
 
   } catch (error) {
-    throw new Error(error.message);
+    if(error.message.includes('Unique constraint failed on the fields')) {
+      throw new Error("Username already taken. Please choose a different username.")
+
+    } else {
+      throw new Error("Error saving user to database.")
+    }
+
   } finally {
-    prisma.$disconnect()
+    prisma.$disconnect();
   }
 }
 
