3194faab9470b0921e182ccee78e329dfe50e5aa
RoyaL <orobertofc@gmail.com>
2023-09-04 21:23:50 -0300

    Changed the HTTP response status code for errors from dynamic to a fixed 404 to ensure a consistent response on failed requests. The error message was also removed from the JSON response

--- app.js
+++ app.js
@@ -42,8 +42,7 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  res.status(err.status || 500);
-  res.json({ "Error": + err.message });
+  res.status(404);
 });
 
 module.exports = app;
