2ca84593f801b42a1a0c100ae20d0e6e3e069782
RoyaL <orobertofc@gmail.com>
2023-09-11 13:04:18 -0300

    Update refresh_token to verify token validity
    The refresh_token method in JWT has been updated to verify the validity of the access token in addition to its existing checks.

--- src/REST/JWT/refresh_token.ts
+++ src/REST/JWT/refresh_token.ts
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
 import createToken from "./create_token.js";
 import getUserById from "./SQL/get_user_by_id.js";
-import {accessToken} from "../../types/token_object.js";
+import {accessToken} from "../../interfaces/token_object.js";
+import checkAccessToken from "../helpers/database/mongoDB/check_token";
 
 
 /**
@@ -13,10 +14,13 @@ import {accessToken} from "../../types/token_object.js";
  */
 async function refreshToken(oldRefreshToken: string): Promise<[string, string]> {
   try {
-    const token: accessToken = jwt.verify(oldRefreshToken, process.env.JWT_REFRESH_SECRET) as accessToken
+    const token = <accessToken> jwt.verify(oldRefreshToken, process.env.JWT_REFRESH_SECRET)
 
     const user = await getUserById(token.id);
     if (!user) throw new Error("User does not exist");
+
+    const tokenValid: boolean = await checkAccessToken(oldRefreshToken);
+    if (!tokenValid) throw new Error("Token is not valid");
 
     const [newAccessToken, newRefreshToken] = await createToken(user);
     return [newAccessToken, newRefreshToken];
