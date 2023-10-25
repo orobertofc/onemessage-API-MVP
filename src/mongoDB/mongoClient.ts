aeba1143c94492cc77899edd4b1d678c3db37363
RoyaL <orobertofc@gmail.com>
2023-10-25 14:11:26 -0300

    Removed old procedural code

--- src/mongoDB/mongoClient.ts
+++
@@ -1,6 +0,0 @@
-import { MongoClient } from "mongodb";
-import "dotenv/config";
-
-const mongo: MongoClient = new MongoClient(process.env.MONGODB);
-const mongoClient: MongoClient = await mongo.connect();
-export default mongoClient;
--- src/prisma/prismaClient.ts
+++
@@ -1,5 +0,0 @@
-import {PrismaClient} from "@prisma/client";
-import "dotenv/config";
-
-const prismaClient:PrismaClient = new PrismaClient();
-export default prismaClient;
